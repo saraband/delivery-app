86997881fa4e96345a48f58832fdb1b7979318e1
saraband <yassine.hermellin@gmail.com>
2018-12-08 19:21:49 +0100

    Updated GraphQL schema

--- src/server/schema.js
+++ src/server/schema.js
@@ -1,5 +1,6 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import db from './models';
+import worldCities from './data/world-cities';
 import { Op } from 'sequelize';
 import GraphQLJSON from 'graphql-type-json';
 
@@ -26,11 +27,18 @@ const typeDefs = `
     ingredients: [String]
   }
   
+  type City {
+    id: ID!
+    name: String!
+    country: String!
+  }
+  
   type Query {
     restaurantsList: [Restaurant!]
     restaurant (restaurantId: ID): Restaurant
     productsList (restaurantId: ID): [Product]
     product (id: ID): Product
+    citiesList (name: String): [City]
   }
   
   type schema {
@@ -46,7 +54,18 @@ const resolvers = {
   Product: {
     restaurant: (product) => product.getRestaurant()
   },
+  City: {
+    id: (city) => city.geonameid
+  },
   Query: {
+    citiesList: (_, { name }) => {
+      const then = Date.now();
+      const filtered = worldCities.filter(city => {
+        return city.name.toLowerCase().includes(name.toLowerCase());
+      });
+      console.log('Searching for \'' + name + '\' took ' + (Date.now() - then) / 1000 + 'ms');
+      return filtered;
+    },
     restaurant: (_, { id }) => db.restaurant.findOne({
       where: {
         [Op.eq]: id
