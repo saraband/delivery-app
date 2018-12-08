a4078edbf78fce344fb1fe75ca34b9d6dfadeacf
saraband <yassine.hermellin@gmail.com>
2018-12-08 10:24:29 +0100

    Re-implemented graphQL schema

--- src/server/schema.js
+++ src/server/schema.js
@@ -1,152 +1,64 @@
-import {
-  GraphQLObjectType,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLList,
-  GraphQLSchema
-} from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
 import db from './models';
+import { Op } from 'sequelize';
 
-const User = new GraphQLObjectType({
-  name: 'User',
-  description: 'User',
-  fields: () => ({
-    id: {
-      type: GraphQLInt,
-      resolve: user => user.id
-    },
-    name: {
-      type: GraphQLString,
-      resolve: user => user.username
-    },
-    password: {
-      type: GraphQLString,
-      resolve: user => user.password
-    },
-    email: {
-      type: GraphQLString,
-      resolve: user => user.email
-    }
-  })
-});
+const typeDefs = `
+  type Restaurant {
+    id: ID!
+    name: String!
+    rating: Int!
+    products: [Product!]
+  }
+  
+  type Product {
+    id: ID!
+    name: String!
+    price: Int!
+    restaurant: Restaurant!
+  }
+  
+  type Query {
+    restaurantsList: [Restaurant!]
+    restaurant (restaurantId: ID): Restaurant
+    productsList (restaurantId: ID): [Product]
+    product (id: ID): Product
+  }
+  
+  type schema {
+    query: Query
+  }
+`;
 
-const Restaurant = new GraphQLObjectType({
-  name: 'Restaurant',
-  description: 'Restaurant',
-  fields: () => ({
-    id: {
-      type: GraphQLInt,
-      resolve: restaurant => restaurant.id
-    },
-    name: {
-      type: GraphQLString,
-      resolve: restaurant => restaurant.name
-    },
-    rating: {
-      type: GraphQLInt,
-      resolve: restaurant => restaurant.rating
-    },
-    products: {
-      type: new GraphQLList(Product),
-      resolve: restaurant => restaurant.getProducts()
-    }
-  })
-});
-
-const Product = new GraphQLObjectType({
-  name: 'Product',
-  description: 'Product',
-  fields: () => ({
-    id: {
-      type: GraphQLInt,
-      resolve: product => product.id
-    },
-    name: {
-      type: GraphQLString,
-      resolve: product => product.name
-    },
-    price: {
-      type: GraphQLString,
-      resolve: product => product.price
-    },
-    restaurant: {
-      type: Restaurant,
-      resolve: product => product.getRestaurant()
-    }
-  })
-});
-
-const Query = new GraphQLObjectType({
-  name: 'Query',
-  description: 'This is a root query',
-  fields: () => ({
-    restaurantsList: {
-      type: new GraphQLList(Restaurant),
-      args: {
-        id: {
-          type: GraphQLInt
-        },
-        name: {
-          type: GraphQLString
-        }
-      },
-      resolve: (root, args) => db.restaurant.findAll({ where: args })
-    },
-    productsList: {
-      type: new GraphQLList(Product),
-      args: {
-        id: {
-          type: GraphQLInt
-        },
+const resolvers = {
+  Restaurant: {
+    products: (restaurant) => restaurant.getProducts()
+  },
+  Product: {
+    restaurant: (product) => product.getRestaurant()
+  },
+  Query: {
+    restaurant: (_, { id }) => db.restaurant.findOne({
+      where: {
+        [Op.eq]: id
+      }
+    }),
+    product: (_, { id }) => db.restaurant.findOne({
+      where: {
+        [Op.eq]: id
+      }
+    }),
+    restaurantsList: () => db.restaurant.findAll(),
+    productsList: (_, { restaurantId }) => db.product.findAll({
+      where: {
         restaurantId: {
-          type: GraphQLString
+          [Op.eq]: restaurantId
         }
-      },
-      resolve: (root, args) => db.product.findAll({ where: args })
-    },
-    usersList: {
-      type: new GraphQLList(User),
-      args: {
-        id: {
-          type: GraphQLInt
-        },
-        name: {
-          type: GraphQLString
-        },
-        email: {
-          type: GraphQLString
-        }
-      },
-      resolve: (root, args) => db.product.findAll({ where: args })
-    }
-  })
+      }
+    })
+  }
+};
+
+export default makeExecutableSchema({
+  typeDefs,
+  resolvers,
 });
-
-const Mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'This is a mutation',
-  fields: () => ({
-    createUser: {
-      type: User,
-      args: {
-        username: {
-          type: GraphQLString
-        },
-        email: {
-          type: GraphQLString
-        },
-        password: {
-          type: GraphQLString
-        }
-      },
-      resolve: (source, args) => db.user.build({ ...args }).save()
-    }
-  })
-})
-
-const Schema = new GraphQLSchema({
-  query: Query,
-  mutation: Mutation
-});
-
-export default Schema;
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export default () => console.log('babel-module-resolver works !');
